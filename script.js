12bee1b2096ca4db7eded869d2ce7b555e43544b
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-05-03 14:19:15 -0300

    function that saves local storage theme

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-let toggleIsOn = false
-
-themeSwitch = () => {
-    let body = document.querySelector("body")
+function app() {
     let toggle = document.querySelector(".input-slider")
-    let themeColor = body.style
+    let input = document.querySelector(".slider")
+    let body = document.querySelector("body")
     let toggleColor = toggle.style
-    
+    let themeColor = body.style
+    let toggleIsOn = 0 
+    let theme
+
     let darkTheme = () => {
-        themeColor.backgroundColor = "var(--dark)";
         toggleColor.backgroundColor = "var(--light)";
+        themeColor.backgroundColor = "var(--dark)";
         themeColor.color = "var(--light)";
+        theme = "dark"
+        input.value = 0
+        localStorage.setItem("pageTheme", JSON.stringify(theme))
     }
 
     let lightTheme = () => {
-        themeColor.backgroundColor = "var(--light)";
         toggleColor.backgroundColor = "var(--dark)";
+        themeColor.backgroundColor = "var(--light)";
         themeColor.color = "var(--dark)";
+        theme = "light"
+        input.value = 1
+        localStorage.setItem("pageTheme", JSON.stringify(theme))
     }
 
-    if(toggleIsOn == false) {
-        toggleIsOn = true
-        console.log(`Dark theme active ${toggleIsOn}`)
-    }else {
-        toggleIsOn = false
-        console.log(`Dark theme active ${toggleIsOn}`)
+    themeSwitch = () => {
+        if(toggleIsOn == 0) {
+            toggleIsOn = 1
+        }else {
+            toggleIsOn = 0
+        }
+
+        let active = toggleIsOn ?  darkTheme() : lightTheme();
     }
 
-    let active = toggleIsOn ?  darkTheme() : lightTheme();
+    let getTheme = JSON.parse(localStorage.getItem("pageTheme"))
+
+    if(getTheme == "dark") {
+        darkTheme()
+        toggleIsOn = 1
+    }
 }
+
+app()
